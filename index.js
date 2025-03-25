9f7ada0af5f1a484f093cc8491cd9f85ccc919f7
Justintutu00 <jstntutu3@gmail.com>
2025-03-25 12:58:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-// Define companyName as a constant
 const companyName = "Scuber";
-
-// Define mostProfitableNeighborhood using let
 let mostProfitableNeighborhood = "Chelsea";
-
-// Define companyCeo using let
 let companyCeo = "Susan Smith";
 
-// Attach variables to global scope so tests can access them
 module.exports = { companyName, mostProfitableNeighborhood, companyCeo };
